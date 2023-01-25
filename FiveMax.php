be8449cb69a15258fa17ea18c6c4ca7afe8b08dd
Aksh Desai <dev.akshdesai@gmail.com>
2023-01-25 14:27:53 +0530

    Added practical 1-3, 5 and 6

---
+++ FiveMax.php
@@ -0,0 +1,12 @@
+<html>
+    <head><title>Document</title></head>
+    <body>
+        <?php
+        $a = 10;
+        $b = 20;
+        $c = 30;
+
+        echo max($a, $b, $c);
+        ?>
+    </body>
+</html>
