0c64f749d44ec673e4022e95b89a40b04b8ff757
AkshDesai04 <dev.akshdesai@gmail.com>
2023-02-01 20:44:55 +0530

    Moved files

---
+++ Other Files/Exp3.php
@@ -0,0 +1,20 @@
+<html>
+    <head><title>Document</title></head>
+    <body>
+        <?php
+            $a = 10;
+            $b = 20;
+            echo "Sum = " . ($a + $b) . "<br>";
+            echo "Dif = " . ($a - $b) . "<br>";
+            echo "Pro = " . ($a * $b) . "<br>";
+            echo "Div = " . ($a / $b) . "<br>";
+            echo "Mod = " . ($a % $b) . "<br>";
+
+            if($a > $b)             echo "A is greater" . "<br>";
+            elseif($b > $a)         echo "B is grater" . "<br>";
+            else                    echo "They are equal" . "<br>";
+
+            if($a < $b && $b > $a)  echo "Yes, math works" . "<br>";
+        ?>
+    </body>
+</html>
---
+++ Other Files/OneHello.php
@@ -0,0 +1,8 @@
+<html>
+    <head><title>Document</title></head>
+    <body>
+        <?php
+            echo "Welcome";
+        ?>
+    </body>
+</html>
