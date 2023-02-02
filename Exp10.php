43228f57ff7151f0625f34eee30c6ef1289f2c7d
AkshDesai04 <dev.akshdesai@gmail.com>
2023-02-02 19:18:32 +0530

    Added Exp10 and index

---
+++ Exp10.php
@@ -0,0 +1,9 @@
+<?php
+    $num = Array(1, 2, 3);
+    $mlt = Array(Array(1, 2), Array(3, 4));
+    $ass = Array("name" => "Aksh", "ID" => "6030");
+
+    print_r($num);
+    print_r($mlt);
+    print_r($ass);
+?>
